d7b9b1e42ed5c2e2d4e454bb94598a10c179c61e
Luis David Garcia Lunar <ldgl1215@gmail.com>
2021-03-17 00:45:53 -0400

    fixing port

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const app = require('./index');
 const config = require('../config')
 
 
-const server =app.listen(config.api.port, () => {
+const server =app.listen(process.env.PORT || config.api.port, () => {
     console.log('Listen at', config.api.port)
 })
 
