b34359123d7cf5d8ef201077317e8289f4b25684
Luis David Garcia Lunar <ldgl1215@gmail.com>
2021-03-17 00:29:35 -0400

    init commit

---
+++ api/server.js
@@ -0,0 +1,9 @@
+const app = require('./index');
+const config = require('../config')
+
+
+const server =app.listen(config.api.port, () => {
+    console.log('Listen at', config.api.port)
+})
+
+module.exports = server
